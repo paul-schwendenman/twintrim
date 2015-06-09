623e67aacdb92ce3c8afde4dbfd851823661310b
Paul Schwendenman <schwendenman.paul@gmail.com>
2015-06-09 00:00:25 -0400

    Added docstrings

--- copycrusher.py
+++ copycrusher.py
@@ -6,16 +6,27 @@ import itertools
 import re
 from collections import defaultdict, namedtuple
 
+
 Filename = namedtuple('Filename', ['name', 'base', 'ext', 'path'])
 
 
 def create_filenames(filenames, root):
+    '''
+    Makes a genrator that yields Filename objects
+
+    Filename objects are a helper to allow multiple representations
+    of the same file to be transfered cleanly.
+    '''
     for filename in filenames:
         yield Filename(filename, *os.path.splitext(filename),
                        path=os.path.join(root, filename))
 
 
 def generate_checksum(filename):
+    '''
+    A helper function that will generate the
+    check sum of a file.
+    '''
     md5 = hashlib.md5()
     with open(filename, 'rb') as f:
         for chunk in iter(lambda: f.read(128 * md5.block_size), b''):
@@ -24,20 +35,45 @@ def generate_checksum(filename):
 
 
 def is_substring(string1, string2):
+    '''
+    Returns a match if one string is a substring of the other
+
+    For example::
+
+        is_substring('this', 'this1') -> True
+        is_substring('that1', 'that') -> True
+
+    But::
+
+        is_substring('that', 'this')  -> False
+
+    '''
     length = min(len(string1), len(string2))
     return string1[:length] == string2[:length]
 
 
 def compare_filename_name(file1, file2):
+    '''
+    Uses is_substring to determine if both base and extension match.
+    '''
     return is_substring(file1.base, file2.base) and is_substring(file1.ext,
                                                                  file2.ext)
 
 
 def compare_filename_checksum(file1, file2):
+    '''
+    Uses the generate_checksum function to compare two files
+    '''
     return generate_checksum(file1.path) == generate_checksum(file2.path)
 
 
 def pick_basename(file1, file2):
+    '''
+    This convience will help to find the shorter (often better) filename
+
+    It picks "file.txt" over "file (1).txt", but beware it also picks
+    "f.txt" over "file.txt".
+    '''
     if len(file1.name) > len(file2.name):
         return file2, file1
     else:
@@ -45,6 +81,10 @@ def pick_basename(file1, file2):
 
 
 def generate_checksum_dict(filenames):
+    '''
+    This function will create a dictionary of checksums mapped to
+    a list of filenames.
+    '''
     checksum_dict = defaultdict(list)
 
     for filename in filenames:
@@ -54,6 +94,10 @@ def generate_checksum_dict(filenames):
 
 
 def generate_filename_dict(filenames):
+    '''
+    This function will create a dictionary of filename parts mapped to a list
+    of the real filenames.
+    '''
     filename_dict = defaultdict(list)
 
     regex = re.compile(r'(^.+?)( \((\d)\))*(\..+)$')
@@ -67,8 +111,9 @@ def generate_filename_dict(filenames):
 
 
 def main(path, no_action, recursive, generate_dict, compare_filename):
-    from pprint import pprint
-
+    '''
+    This function handles all options and steps through the directory
+    '''
     for root, dirs, filenames in os.walk(path):
         if not recursive and root != path:
             continue
