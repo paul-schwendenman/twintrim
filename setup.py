181785b88a29ff519481bf23cf0a731ac19bbac9
Paul Schwendenman <schwendenman.paul@gmail.com>
2015-07-05 01:25:58 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name='twintrimmer',
-      version='0.2',
+      version='0.3',
       description='tool for removing duplicate files',
       author='Paul Schwendenman',
       url='https://github.com/paul-schwendenman/twintrim',
