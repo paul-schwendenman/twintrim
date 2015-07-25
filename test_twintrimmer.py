4b9e3f948e53da6b538cea412010ab9efaab2530
Paul Schwendenman <schwendenman.paul@gmail.com>
2015-07-25 17:33:38 -0400

    Added skipped test for generate_checksum
    mock_open currently has a bug in it that prevents it from functioning
    properly when dealing with byte strings.  The hashlib library requires a
    byte string for calculation.  Until this is fixed in python our options are
    to ignore the test, create our own mock_open based on the patch or mock the
    hashlib function as well.  For the plan is to skip this new test and write a
    second test that will mock the hashlib call.
    
    Current(ish) code:
    https://github.com/python/cpython/blob/master/Lib/unittest/mock.py#L2290
    
    Bug:
    https://bugs.python.org/issue23004
    https://bugs.python.org/file37510/mock-open-allow-binary-data-fix-formatting.patch

--- test_twintrimmer.py
+++ test_twintrimmer.py
@@ -1,5 +1,7 @@
 import unittest
 import twintrimmer
+from unittest.mock import patch, mock_open
+import builtins
 
 class TestSomeThings(unittest.TestCase):
     def setUp(self):
@@ -29,6 +31,18 @@ class TestCreateFilenames(unittest.TestCase):
         list_output = list(twintrimmer.create_filenames(filenames, root))
         self.assertEqual(len(list_output), 0)
 
+class TestGenerateChecksum(unittest.TestCase):
+  def setUp(self):
+      pass
+
+  @unittest.skip('Skip: bug in python issue23004')
+  def test_generate_checksum(self):
+      m = mock_open(read_data='bibble'.encode('utf-8'))
+      with patch('builtins.open', m, create=True):
+          checksum = twintrimmer.generate_checksum('foo')
+
+      m.assert_called_once_with('foo')
+      self.assertEqual(checksum, 'cb1eda4e6df6ff361f9ed94f91ce5386')
 
 
 if __name__ == '__main__':
