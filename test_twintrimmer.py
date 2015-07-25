44209622777e77a5f1fa69aa1482a7eae3700570
Paul Schwendenman <schwendenman.paul@gmail.com>
2015-07-25 14:30:41 -0400

    Added tests for create_filename

--- test_twintrimmer.py
+++ test_twintrimmer.py
@@ -1,15 +1,31 @@
 import unittest
+import twintrimmer
 
 class TestSomeThings(unittest.TestCase):
     def setUp(self):
         pass
-    
+
     def test_this_things(self):
         assert True
-        
+
     def tearDown(self):
         pass
-    
+
+class TestCreateFilenames(unittest.TestCase):
+    def test_one_file(self):
+        filenames = ['test_file.txt']
+        root = '/'
+        list_output = list(twintrimmer.create_filenames(filenames, root))
+        self.assertEqual(len(list_output), 1)
+        self.assertIs(type(list_output[0]), twintrimmer.Filename)
+
+    def test_no_files(self):
+        filenames = []
+        root = '/'
+        list_output = list(twintrimmer.create_filenames(filenames, root))
+        self.assertEqual(len(list_output), 0)
+
+
 
 if __name__ == '__main__':
     unittest.main()
