d701ecac8c9d7450f5cfe59c709f647ed8146773
Paul Schwendenman <schwendenman.paul@gmail.com>
2015-08-13 22:41:52 -0400

    Added first test of new file removal function

--- tests/twintrimmer_test.py
+++ tests/twintrimmer_test.py
@@ -153,6 +153,16 @@ class TestCaseWithFileSystem(fake_filesystem_unittest.TestCase):
         self.fs.CreateFile('examples/underscore/file__1.txt',
                            contents='\n')
 
+class TestRemoveFilesMarkedForDeletion(TestCaseWithFileSystem):
+    def test_no_action_does_no_action(self):
+        bad = twintrimmer.Filename(None, None, None, 'examples/foo (1).txt')
+        best = twintrimmer.Filename(None, None, None, 'examples/foo.txt')
+        twintrimmer.remove_files_marked_for_deletion(
+            bad, best,
+            remove_links=True,
+            no_action=True)
+        self.assertTrue(os.path.exists('examples/foo (1).txt'))
+
 
 @unittest.skip('Skip these tests')
 class TestWalkPath(TestCaseWithFileSystem):
