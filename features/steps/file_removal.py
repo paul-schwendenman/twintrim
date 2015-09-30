35400700b3e79a0f6fb21b260e29acd1ecd3a93d
Paul Schwendenman <schwendenman.paul@gmail.com>
2015-09-30 18:58:51 -0400

    Added run_program function to capture output
    Capture the exit code, stdout and stderr.
    
    Python 3.5
    -----------
    completed = subprocess.run(command,
                               stdout=subprocess.PIPE,
                               stderr=subprocess.PIPE, shell=True)
    return completed.returncode, completed.stdout, completed.stderr

--- features/steps/file_removal.py
+++ features/steps/file_removal.py
@@ -1,7 +1,21 @@
+'''
+Acceptance style tests
+'''
+#pylint: disable=missing-docstring, function-redefined
 from behave import given, when, then
 import os
 import shutil
 import subprocess
+import sys
+
+def run_program(command):
+    doit = subprocess.Popen(command, universal_newlines=True, shell=True,
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    (done, fail) = doit.communicate()
+    code = doit.wait()
+    sys.stdout.write(done)
+    sys.stderr.write(fail)
+    return code, done, fail
 
 @given(u'we have two files "{filename1}" and "{filename2}"')
 def step_impl(context, filename1, filename2):
@@ -26,8 +40,8 @@ def step_impl(context, filename):
 
 @when(u'we run "{program}" with no args')
 def step_impl(context, program):
-    subprocess.call([program, context.path])
+    run_program(' '.join([program, context.path]))
 
 @when(u'we run "{program}" with args: "{args}"')
 def step_impl(context, program, args):
-    subprocess.call([program, args, context.path])
+    run_program(' '.join([program, args, context.path]))
